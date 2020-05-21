1f9896c648735071d63d78cee62bdd3c470bcaaf
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-05-21 15:28:59 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,7 +77,7 @@ client.on("message", async message => {
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
     if(!message.member.roles.some(r=>["tank"].includes(r.name)) )
-      return message.reply("You have no power here");    const sayMessage = args.join(" ");
+      return message.reply("you have no power here");    const sayMessage = args.join(" ");
     // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     message.delete().catch(O_o=>{}); 
     // And we get the bot to say the thing: 
