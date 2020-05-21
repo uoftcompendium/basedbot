c07ea4d42b37d381b14fbce2d8e2b51ec2b23f95
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-05-20 20:00:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ client.on('message', message => {
        message.channel.send("cope " + message.author);
        }
      if (message.content === 'cringe') {
-       message.channel.send("https://cdn.discordapp.com/attachments/701688940465225829/712796621137772654/SPOILER_SPOILER_1589297889284.jpg");
+       message.channel.send("so cringe");
        }
 
 });
