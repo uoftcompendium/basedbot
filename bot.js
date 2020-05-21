4ebcd59989cae07b88c35e1c1803ea47e8084d02
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-05-21 15:30:05 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,15 +35,6 @@ client.on("message", async message => {
     m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
   }
   
-  if(command === "say") {
-    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
-    // To get the "message" itself we join the `args` back into a string with spaces: 
-    const sayMessage = args.join(" ");
-    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
-    message.delete().catch(O_o=>{}); 
-    // And we get the bot to say the thing: 
-    message.channel.send(sayMessage);
-  }
   
   if(command === "kick") {
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
