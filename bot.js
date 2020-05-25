c66323f364cf019f2f8d468fbf0416649de29c5c
uoftcompendium <64548255+uoftcompendium@users.noreply.github.com>
2020-05-25 13:57:19 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,6 +74,17 @@ client.on("message", async message => {
     // And we get the bot to say the thing: 
     message.channel.send(sayMessage);
   }
+    
+  if(command === " load config") {
+    // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
+    // To get the "message" itself we join the `args` back into a string with spaces: 
+    if(!message.member.roles.some(r=>["tank"].includes(r.name)) )
+      return message.reply("you have no power here");    const sayMessage = args.join(" ");
+    // Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
+    message.delete().catch(O_o=>{}); 
+    // And we get the bot to say the thing: 
+    message.channel.send("Config loaded. State parameters.);
+  }
   
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
